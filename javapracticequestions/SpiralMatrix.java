ea349590a400f0e0b10f07be6588c5d1943a50e7
kshitija15 <52289692+kshitija15@users.noreply.github.com>
2022-02-07 12:02:44 +0530

    Update SpiralMatrix.java

--- javapracticequestions/SpiralMatrix.java
+++ javapracticequestions/SpiralMatrix.java
@@ -1,75 +1,66 @@
 package javapracticequestions;
 
-public class SpiralMatrix {
-//Java program to print a given matrix in spiral form 
-    // Function print matrix in spiral form 
-    static void spiralPrint(int m, int n, int a[][]) 
-    { 
-        int i, k = 0, l = 0; 
-        /*  k - starting row index 
-        m - ending row index 
-        l - starting column index 
-        n - ending column index 
-        i - iterator 
-        */
-           
-        while (k < m && l < n) 
-        { 
-            // Print the first row from the remaining rows 
-            for (i = l; i < n; ++i) 
-            { 
-                System.out.print(a[k][i]+" "); 
-            } 
-            k++; 
-   
-            // Print the last column from the remaining columns  
-            for (i = k; i < m; ++i) 
-            { 
-                System.out.print(a[i][n-1]+" "); 
-            } 
-            n--; 
-   
-            // Print the last row from the remaining rows */ 
-            if ( k < m) 
-            { 
-                for (i = n-1; i >= l; --i) 
-                { 
-                    System.out.print(a[m-1][i]+" "); 
-                } 
-                m--; 
-            } 
-   
-            // Print the first column from the remaining columns */ 
-            if (l < n) 
-            { 
-                for (i = m-1; i >= k; --i) 
-                { 
-                    System.out.print(a[i][l]+" "); 
-                } 
-                l++;     
-            }         
-        } 
-    } 
-      
-    public static void main (String[] args)  
-    { 
-        int R = 3; 
-        int C = 6; 
+import java.util.Scanner;
 
-        int a[][] = { {1,  2,  3,  4,  5,  6}, 
-                      {7,  8,  9,  10, 11, 12}, 
-                      {13, 14, 15, 16, 17, 18} 
-                    }; 
+public class SpiralMatrixNew {
+
+	public static void main(String[] args) {
+		int i,j;
+		System.out.println("Enter the value of n:");
+        Scanner s=new Scanner(System.in);
+        int n=s.nextInt();
         
-              for( R=0;R<3;R++) {
-            	  for(C=0;C<6;C++) {
-            		  System.out.print(a[R][C] + " ");
-            	  }
-            	  System.out.println(" ");
-              }
-        System.out.println("Spiral Matrix :");
+        int minRow=0;
+        int minCol=0;
+        int maxRow=n-1;
+        int maxCol=n-1;
+        int currentNumber=1;
+        
+        int [][] matrix=new int [n][n];
+        
+        while(minRow<=maxRow && minCol<=maxCol) {
+        	
+        	//left to right
+        	for( i = minRow , j = minCol; j <= maxCol; j++) {
+                
+        		matrix[i][j]=currentNumber;
+        		currentNumber++;
+        		}
+        	minRow++;
+        	    
+        	//top to bottom
+        	for(i = minRow,j = maxCol; i <= maxRow; i++) {
+                
+        		matrix[i][j]=currentNumber;
+        		currentNumber++;
+        		}
+        	maxCol--;
+        	
+        	//right to left
+        	for(i = maxRow,j = maxCol; j >= minCol; j--) {
+                
+        		matrix[i][j]=currentNumber;
+        		currentNumber++;
+        	}
+        	maxRow--;
+        	
+        	//bottom to top
+        	for(i = maxRow, j = minCol; i >= minRow; i--) {
+        		matrix[i][j]=currentNumber;
+        		currentNumber++;
+        	}
+        	minCol++;
+        		
+        }
+        
+        for(i = 0; i <n; i++) {
+        	for(j = 0; j<n; j++) {
+                
+        		System.out.print( matrix[i][j] + " ");
 
-        spiralPrint(R,C,a); 
-    } 
-} 
-  
+        	}
+        	System.out.println(" ");
+        }
+	}
+
+}
